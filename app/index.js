22df46f6073283818b09233e00edc420f53b53b5
EraldoAbedini <abedinieraldo@gmail.com>
2022-06-14 22:10:06 +0200

    main file

--- app/index.js
+++ app/index.js
@@ -1,5 +1,7 @@
 const fs = require("fs");
 const conc = require("/mnt/c/Users/User/Desktop/ProjectNode/app/concat.js");
+const dis = require("/mnt/c/Users/User/Desktop/ProjectNode/app/promise.js");
+
 
 const array = [10, 32, 36, 76];
 const list = JSON.parse(fs.readFileSync("/mnt/c/Users/User/Desktop/ProjectNode/app/numbers.json")).numbers;
@@ -8,14 +10,8 @@ const bothLs = conc.Concated(array, list);
 //synchronized
 console.log("array: " + array + "\t" + "bothLs: " + bothLs);
 
-//asynchronized
-let promise = new Promise((resolve, reject) => {
-    (bothLs.length > array.length) ? resolve("bothLs will be displayed: ") : reject("bothLs can't be displayed!")
-})
-promise.then(
-    (message) => console.log(message + bothLs)
-).catch((message) => console.log(message))
-
+//promise
+dis.Display(bothLs, array);
 
 setTimeout(() => console.log("array: " + array), 3000)
 setTimeout(() => console.log("bothLs: " + bothLs), 2000)
