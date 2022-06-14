c37847c0f4704b1d86ec7936a50bc6fe49e7a1ab
EraldoAbedini <abedinieraldo@gmail.com>
2022-06-14 20:52:08 +0200

    main file

--- app/index.js
+++ app/index.js
@@ -1,11 +1,9 @@
 const fs = require("fs");
 const conc = require("/mnt/c/Users/User/Desktop/ProjectNode/app/concat.js");
-const sum = require("/mnt/c/Users/User/Desktop/ProjectNode/app/arraysSum.js");
-const sumOSq = require("/mnt/c/Users/User/Desktop/ProjectNode/app/squares.js");
 
 const array = [10, 32, 36, 76];
 const list = JSON.parse(fs.readFileSync("/mnt/c/Users/User/Desktop/ProjectNode/app/numbers.json")).numbers;
-const bothLs = conc.concat(array, list);
+const bothLs = conc.Concated(array, list);
 
 //synchronized
 console.log("array: " + array);
@@ -20,9 +18,5 @@ promise.then(
 ).catch((message) => console.log(message))
 
 
-setTimeout(() => {
-    console.log("array: " + array);
-}, 3000)
-setTimeout(() => {
-    console.log("bothLs: " + bothLs);
-}, 2000)
+setTimeout(() => console.log("array: " + array), 3000)
+setTimeout(() => console.log("bothLs: " + bothLs), 2000)
